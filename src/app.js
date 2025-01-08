dffd53ad5bbf47f8e12a4c0e233579dffaf2568e
FernandoJavier1214 <ferchojavier1214@gmail.com>
2025-01-08 04:41:03 +0000

    FernandoJavier

--- src/app.js
+++ src/app.js
@@ -7,5 +7,23 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let who = ["El perro ", "Mi abuela ", "El correo ", "Mi pajaro "];
+  let action = ["comió ", "hizo pis ", "aplastó ", "rompio "];
+  let what = ["mi tarea ", "mi telefono ", "el carro "];
+  let when = [
+    "antes de la clase ",
+    "cuando estaba durmiendo ",
+    "mientras estaba dormido ",
+    "durante el desayuno ",
+    "mientras estaba orando "
+  ];
+
+  let rd1 = Math.floor(Math.random() * who.length);
+  let rd2 = Math.floor(Math.random() * action.length);
+  let rd3 = Math.floor(Math.random() * what.length);
+  let rd4 = Math.floor(Math.random() * when.length);
+
+  document.querySelector("#excuse").innerHTML =
+    who[rd1] + action[rd2] + what[rd3] + when[rd4];
 };
